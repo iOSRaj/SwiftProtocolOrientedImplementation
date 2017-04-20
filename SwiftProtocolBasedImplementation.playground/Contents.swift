86d4f396d0ec89b7f2623b8d60df5b1af22ab092
Raj <rajkumargkr@gmail.com>
2017-04-20 16:13:41 +0200

    Modified the names and Conventions

--- SwiftProtocolBasedImplementation.playground/Contents.swift
+++ SwiftProtocolBasedImplementation.playground/Contents.swift
@@ -1,44 +1,26 @@
 
-
-//Application 1
-struct Application1: DisplayNameDelegate {
-    func displayName(name: String) -> String {
-        print("App1 \(name)")
-        return "Appo "+name+" Eppo 2.0"
-    }
-}
-
-//Application 2
-struct Application2: DisplayNameDelegate {
-    func displayName(name: String) -> String{
-        print("App2 \(name)")
-        return "Say hello to " + name
-    }
-}
-
-
 //Common Function
-protocol DisplayNameDelegate {
+protocol DisplayFullName {
     func displayName(name: String)-> String
 }
 
 
 struct Person {
-    var displayNameDelegate: DisplayNameDelegate
+    var displayFullName: DisplayFullName
     var familyName: String?
     
-    init(displayNameDelegate: DisplayNameDelegate) {
-        self.displayNameDelegate = displayNameDelegate
+    init(displayFullName: DisplayFullName) {
+        self.displayFullName = displayFullName
     }
     
     var firstName = "" {
         didSet {
-            displayNameDelegate.displayName(name: getFullName())
+            displayFullName.displayName(name: getFullName())
         }
     }
     var lastName = "" {
         didSet {
-            displayNameDelegate.displayName(name: getFullName())
+            displayFullName.displayName(name: getFullName())
         }
     }
     
@@ -47,28 +29,42 @@ struct Person {
     }
     
     var greet:String {
-        return  displayNameDelegate.displayName(name: getFullName())
+        return  displayFullName.displayName(name: getFullName())
+    }
+}
+
+
+//Application 1
+struct Application1: DisplayFullName {
+    func displayName(name: String) -> String {
+        return "Say Ola to" + name
+    }
+}
+
+//Application 2
+struct Application2: DisplayFullName {
+    func displayName(name: String) -> String{
+        return "Say Hello to" + name
     }
 }
 
 
 //------------
 //Application1 Implementaion
-var app1Delegate = Application1()
-var person = Person(displayNameDelegate: app1Delegate)
-person.firstName = "Kabali"
-person.lastName = "Swaran"
-person.familyName = "Rajni"
-person.greet
+var app1Information = Application1()
+var person = Person(displayFullName: app1Information)
+person.firstName = "Steve"
+person.lastName = "Smith"
+person.familyName = "Sam"
+print(person.greet)
 
 
 //Application 2 Implementation
-var app2Delegate = Application2()
+var app2Information = Application2()
 //UI Class
-var person2 = Person(displayNameDelegate: app2Delegate)
-person2.firstName = "Robo"
-person2.lastName = "Raj"
-person2.greet
+var person2 = Person(displayFullName: app2Information)
+person2.firstName = "Tim"
+person2.lastName = "Cook"
+print(person2.greet)
 //-------------------
-
 
